b76471968b806a4aa1379b5dc870b3cc86d09567
Brock Halladay <bhalladay@instructure.com>
2018-06-13 14:27:21 -0600

    tested user creation

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,6 +4,8 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
+
 const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
 beforeEach(populateUsers);
@@ -168,4 +170,81 @@ describe('PATCH /todos/:todo_id', ()=>{
       }).catch((err)=>done(err))
     });
   });
-})
+});
+  describe('GET /users/me', ()=>{
+    it('should return the user if authenticated', (done)=>{
+      request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      }).end(done);
+    });
+    it('should return a 401 if not authenticated', (done)=>{
+      request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect((res)=>{
+        expect(res.body).toEqual({});
+      }).end(done);
+    });
+  });
+
+  describe('POST /users', ()=>{
+    it('should create a user', (done)=>{
+      let email = `example@notanemailaddress.com`;
+      let password = `superman123`;
+      let username = `example`;
+
+      request(app)
+      .post('/users')
+      .send({email, password, username})
+      .expect(200)
+      .expect((res)=>{
+        expect(res.headers['x-auth']).toBeTruthy();
+        expect(res.body._id).toBeTruthy();
+        expect(res.body.email).toBe(email);
+      }).end((err)=>{
+        if(err){
+          return done(err);
+        }
+
+        User.findOne({email}).then((user)=>{
+          expect(user).toBeTruthy();
+          expect(user.password).not.toBe(password);
+          done();
+        })
+      });
+    });
+    it('should return validation errors when request is invalid', (done)=>{
+      let email = `asdfasdfasd`;
+
+      request(app)
+      .post('/users')
+      .send({email})
+      .expect(400)
+      .end(done);
+    });
+    it('should not create a user with the same email as another user', (done)=>{
+      let email = `casey@notanemailaddress.com`;
+      let password = `superman123`;
+      let username = `casey`;
+
+      request(app)
+      .post('/users')
+      .send({email,password,username})
+      .expect(400)
+      .end((err)=>{
+        if(err) {
+          return done(err)
+        }
+
+        User.find({email}).then((arr)=>{
+          expect(arr.length).toBe(1);
+          done();
+        })
+      });
+    });
+  });
