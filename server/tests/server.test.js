a9cfc42851f513310cdd1dacedc172251ba238d6
Brock Halladay <bhalladay@instructure.com>
2018-06-13 13:48:54 -0600

    added seed data in prep for testing user API calls

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,24 +4,10 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
-const {User} = require('./../models/user');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-const todos = [
-  {
-    text: 'First test todo', 
-    _id: new ObjectID()
-  },
-  {
-    text: 'Second test todo',
-    _id: new ObjectID(),
-    status: "in progress"
-  }];
-
-beforeEach((done)=>{
-  Todo.remove({}).then(()=>{
-    return Todo.insertMany(todos);
-  }).then(()=> done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', ()=>{
   it('should create a new todo', (done)=>{
